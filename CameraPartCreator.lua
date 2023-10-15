89f9f7e595ff7d9ba4a3d3adfb4f012fd5442ca3
maxplayz7566 <84992927+maxplayz2746@users.noreply.github.com>
2023-10-15 12:10:39 -0600

    Update CameraPartCreator.lua

--- CameraPartCreator.lua
+++ CameraPartCreator.lua
@@ -1,14 +1,10 @@
--- on clicked function --
-CreatePart.Click:Connect(function()
-	-- Create The Camera Part --
-	local newPart = Instance.new("Part")
-	newPart.Name = "Camera Part"
-	newPart.CanCollide = false
-	newPart.Anchored = true
-	newPart.TopSurface = Enum.SurfaceType.Smooth
-	newPart.BottomSurface = Enum.SurfaceType.Smooth
-	newPart.FrontSurface = Enum.SurfaceType.Motor
-	newPart.Size = Vector3.new(1,1,1)
-	newPart.CFrame = workspace.Camera.CFrame
-	newPart.Parent = workspace
-end)
+local newPart = Instance.new("Part")
+newPart.Name = "Camera Part"
+newPart.CanCollide = false
+newPart.Anchored = true
+newPart.TopSurface = Enum.SurfaceType.Smooth
+newPart.BottomSurface = Enum.SurfaceType.Smooth
+newPart.FrontSurface = Enum.SurfaceType.Motor
+newPart.Size = Vector3.new(1,1,1)
+newPart.CFrame = workspace.Camera.CFrame
+newPart.Parent = workspace
