8dde168211f770c40a26635d2536395c09d05d19
maxplayz7566 <84992927+maxplayz2746@users.noreply.github.com>
2023-10-15 11:47:41 -0600

    Update CameraPartCreator.lua

--- CameraPartCreator.lua
+++ CameraPartCreator.lua
@@ -1,14 +1,4 @@
-local selcetion = game:GetService("Selection")
-
-local toolBar = plugin:CreateToolbar("Camera Positioner")
-local openGuibutton = toolBar:CreateButton("Create Camera Position", "Create Camera Position At Your Current Position", "rbxassetid://11478662607")
-
-local gui = script:WaitForChild("mainGui", 10)
-
-openGuibutton.Click:Connect(function()
-	
-	
-	
+CreatePart.Click:Connect(function()
 	local newPart = Instance.new("Part", workspace)
 	newPart.Name = "Camera Part"
 	newPart.CanCollide = false
@@ -19,5 +9,7 @@ openGuibutton.Click:Connect(function()
 	newPart.Size = Vector3.new(1,1,1)
 	local newScirpt = Instance.new("Script")
 	newScirpt.Parent = newPart
-	newScript.Source = loadstring(game:HttpGet("https://raw.githubusercontent.com/maxplayz2746/For-Some-Roblox-Code/main/CodeForSpawnedCameraParts.lua"))()
+	newScript.Source = [[
+		sc
+	]]
 end)
